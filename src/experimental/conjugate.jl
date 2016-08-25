432c03d06a806224b944002b1198f1da030c6895
Lorenzo Stella <lorenzostella@gmail.com>
2016-08-25 14:24:31 +0200

    added experimental features

---
+++ src/experimental/conjugate.jl
@@ -0,0 +1,25 @@
+# Conjugate function: can we implement call somehow?
+# I guess we would need to invert the subgradient of f, i.e., to have the
+# subgradient of f_star... Maybe one could use AD?
+# The prox is ok anyway, and in the end that's what is needed in algorithms.
+
+"""
+  Conjugate(f::ProximableFunction)
+
+Returns the Fenchel conjugate of a given convex function `f`.
+"""
+
+immutable Conjugate <: ProximableFunction
+  f::ProximableFunction
+end
+
+function prox(g::Conjugate, x::Array, gamma::Float64=1.0)
+  z, v = prox(g.f, x/gamma, 1/gamma)
+  p = x - gamma*z
+  return p, vecdot(p,z) - v
+end
+
+fun_name(g::Conjugate) = string("conjugate of ", fun_name(g.f))
+fun_type(g::Conjugate) = fun_type(g.f)
+fun_expr(g::Conjugate) = string("conjugate of ", fun_expr(g.f))
+fun_params(g::Conjugate) = fun_params(g.f)
---
+++ src/experimental/separableSum.jl
@@ -0,0 +1,24 @@
+immutable SeparableSum <: ProximableFunction
+  N::Int
+  fs::Tuple
+  SeparableSum(fs...) =
+    new(length(fs), fs)
+end
+
+function call(f::SeparableSum, xs...)
+  vs = map(i -> f.fs[i](xs[i]), 1:f.N)
+  return sum(vs)
+end
+
+function prox(f::SeparableSum, xs..., gamma::Float64=1.0)
+  res = map(i -> prox(f.fs[i], xs[i], gamma), (1:f.N...))
+  return map(p -> p[1], res), sum(map(p -> p[2], res))
+end
+
+function Base.show(io::IO, f::SeparableSum)
+  for i = 1:f.N-1
+    println(io, f.fs[i])
+    println(io, "---")
+  end
+  print(io, f.fs[f.N])
+end
