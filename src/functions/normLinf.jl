f62c3fd81c34c3af42427b5f4516b50ee5881138
Lorenzo Stella <lorenzostella@gmail.com>
2016-11-07 18:31:18 +0700

    fixed compatibility with 0.4

--- src/functions/normLinf.jl
+++ src/functions/normLinf.jl
@@ -1,7 +1,5 @@
 # L-infinity norm
 
-typealias NormLinf{R <: Real} Postcomposition{Conjugate{IndBallL1{R}}, R}
-
 """
   NormLinf(λ::Real=1.0)
 
@@ -10,14 +8,16 @@ Returns the function `g(x) = λ*max(abs(x))`, for a nonnegative parameter `λ �
 
 NormLinf{R <: Real}(lambda::R=1.0) = Postcomposition(Conjugate(IndBallL1(one(R))), lambda)
 
+# typealias NormLinf{R <: Real} Postcomposition{Conjugate{IndBallL1{R}}, R}
+
 @compat function (f::Conjugate{IndBallL1{R}}){R <: Real, S <: RealOrComplex}(x::AbstractArray{S,1})
   return vecnorm(x, Inf)/(f.f.r)
 end
 
-fun_name{R <: Real}(f::NormLinf{R}) = "weighted L-infinity norm"
-fun_expr{R <: Real}(f::NormLinf{R}) = "x ↦ λ||x||_∞"
-fun_params{R <: Real}(f::NormLinf{R}) = "λ = $(f.a)"
+fun_name{R <: Real}(f::Postcomposition{Conjugate{IndBallL1{R}}, R}) = "weighted L-infinity norm"
+fun_expr{R <: Real}(f::Postcomposition{Conjugate{IndBallL1{R}}, R}) = "x ↦ λ||x||_∞"
+fun_params{R <: Real}(f::Postcomposition{Conjugate{IndBallL1{R}}, R}) = "λ = $(f.a)"
 
-function prox_naive{T <: RealOrComplex}(f::NormLinf, x::AbstractArray{T,1}, gamma::Real=1.0)
+function prox_naive{T <: RealOrComplex}(f::Postcomposition{Conjugate{IndBallL1}}, x::AbstractArray{T,1}, gamma::Real=1.0)
   return prox_naive(f.g, x, gamma)
 end
