bc49cb82d526828129857ed85340e3acce5fb2f1
nantonel <nantonel@esat.kuleuven.be>
2016-11-24 12:08:14 +0100

    added constructor to separable sum and option to slice variable

--- src/calculus/separableSum.jl
+++ src/calculus/separableSum.jl
@@ -3,22 +3,56 @@
 immutable SeparableSum{P<:ProximableFunction, I<:AbstractArray} <: ProximableFunction
 	prox_col::Vector{P}
 	ind_col::Array{I}
+	dim::Int64
 
-	function (::Type{SeparableSum}){P,I}(a::Vector{P},b::Array{I})
-		new{P,I}(a, b)
+	function (::Type{SeparableSum}){P,I}(a::Vector{P},b::Array{I}, dim::Int64 = 1)
+		if length(a)!= length(b)
+			error("length(prox_col) must conicide with length(ind_col)")
+		else
+			new{P,I}(a, b, dim)
+		end
 	end
 end
 
+function SeparableSum(p::Pair...; dim::Int64 = 1)
+
+	a = Vector{ProximableFunction}(length(p))
+	b = Vector{AbstractArray}(length(p))
+
+	for i in eachindex(p)
+		a[i] = p[i].first
+		b[i]  = p[i].second
+	end
+	return SeparableSum(a, b, dim)
+	
+end
 
 function prox!{T <: RealOrComplex, R <: Real}(f::SeparableSum, x::AbstractArray{T}, 
 					      y::AbstractArray{T}, gamma::R=1.0)
   v = 0.0
+  nd = ndims(x)
+
   for i in eachindex(f.prox_col)
-	  z = y[f.ind_col[i]]
-	  g = prox!(f.prox_col[i],x[f.ind_col[i]],z,gamma)
-	  y[f.ind_col[i]] = z
+	  z = slicedim(y,f.dim,f.ind_col[i])
+	  g = prox!(f.prox_col[i],slicedim(x,f.dim,f.ind_col[i]),z,gamma)
+	  y[[ n==f.dim ? f.ind_col[i] : indices(y,n) for n in 1:nd ]...] = z
 	  v += g
   end
   return v
 
+end
+
+function Base.show(io::IO, f::SeparableSum)  
+
+	println(io,"Separable Sum: \n")
+	if length(f.prox_col)<=4
+		for p in f.prox_col
+			show(p)
+			println(io,"\n")
+		end
+	else
+		show(f.prox_col[1])
+		println(io,"\n .... \n")
+		show(f.prox_col[end])
+	end
 end
--- test/test_SeparableSum.jl
+++ test/test_SeparableSum.jl
@@ -6,11 +6,20 @@ x = randn(100)
 y0 = randn(100)
 y = copy(y0)
 
-prox_col = [NormL1(0.1),NormL2(0.01)]
+prox_col = [NormL1(0.1),IndBallL0(1)]
 ind_col = [1:50,51:100]
+M1 = ones(Bool,size(x))
+M1[51:end] = false
+M2 = M1.==false
 
 f = SeparableSum(prox_col,ind_col)
+#test second constructor and mask
+ff = SeparableSum(prox_col[1]=>M1,prox_col[2]=>M2)
 y,fy = prox(f,x,1.)
+yy,fyy = prox(ff,x,1.)
+
+@test norm(fyy-fy)<1e-11
+@test norm(y-yy)<1e-11
 
 y1,fy1 = prox(prox_col[1],x[ind_col[1]],1.)
 y2,fy2 = prox(prox_col[2],x[ind_col[2]],1.)
@@ -18,8 +27,24 @@ y2,fy2 = prox(prox_col[2],x[ind_col[2]],1.)
 @test norm((fy1+fy2)-fy)<1e-11
 @test norm(y-[y1;y2])<1e-11
 
+X1,X2 = randn(10,10),randn(10,10)
+X = [X1 X2]
 
-X = randn(100,100)
-prox_col = (NormL21(0.1),NormL2(0.01))
-ind_col = (1:50,51:100)
+f = SeparableSum([NormL1(1.),NormL21(0.1)],[1:10,11:20],2 )
+#test second constructor
+ff = SeparableSum(NormL1(1.)=> 1:10, NormL21(0.1)=> 11:20; dim =2 )
 
+y,fy = prox(f,X,1.)
+yy,fyy = prox(ff,X,1.)
+
+@test norm(fyy-fy)<1e-11
+@test norm(y-yy)<1e-11
+
+y1,fy1 = prox(NormL1(1.),X1,1.)
+y2,fy2 = prox(NormL21(0.1),X2,1.)
+
+@test norm((fy1+fy2)-fy)<1e-11
+@test norm(y-[y1 y2])<1e-11
+
+show(ff)
+show(SeparableSum(repmat([NormL1(1.)],6),repmat([1:10],6),2 ))
