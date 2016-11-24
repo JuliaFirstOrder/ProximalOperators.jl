588b96edf6688ca56e72ec0d48804dccb290b87f
Lorenzo Stella <lorenzostella@gmail.com>
2016-11-24 17:27:14 +0100

    corrected SeparableSum

--- src/calculus/separableSum.jl
+++ src/calculus/separableSum.jl
@@ -1,52 +1,49 @@
+# separable sum
 
-immutable SeparableSum{P<: ProximableFunction} <: ProximableFunction
-	prox_col::Vector{P}
+immutable SeparableSum <: ProximableFunction
+	fs::Array{ProximableFunction}
 end
 
-function SeparableSum(pr::Vector{DataType}, lambdas::Vector)
-	prox_col = Vector{ProximableFunction}(length(lambdas)) 
-	if length(pr)!=length(lambdas)
-		error("lambda vector and proximal operator vecor must have the same lenght")
-	else
-		for i in eachindex(pr)
-			prox_col[i] = pr[i](lambdas[i])
-		end
-		return SeparableSum(prox_col)
+@compat function (f::SeparableSum)(x::AbstractArray)
+	sum = 0.0
+  for k in eachindex(f.fs)
+		sum += f.fs[k](x[k])
 	end
-
+	return sum
 end
 
-function prox!{R <: Real}(f::SeparableSum, x::Vector, 
-			  y::Vector, gamma::R=1.0)
-  v = 0.0
-  for i in eachindex(f.prox_col)
-	  g = prox!(f.prox_col[i],x[i],y[i],gamma)
-	  v += g
+function prox!(f::SeparableSum, x::AbstractArray, y::AbstractArray, gamma::Real=1.0)
+  sum = 0.0
+  for k in eachindex(f.fs)
+	  sum += prox!(f.fs[k], x[k], y[k], gamma)
   end
-  return v
-
+  return sum
 end
 
-function prox(f::SeparableSum, x::Vector, gamma::Real=1.0)
-	y = Vector(length(x))
-	for i in eachindex(x)
-		y[i] = similar(x[i])
+function prox(f::SeparableSum, x::AbstractArray, gamma::Real=1.0)
+	y = similar(x)
+	for k in eachindex(x)
+		y[k] = similar(x[k])
 	end
 	fy = prox!(f, x, y, gamma)
 	return y, fy
 end
 
-function Base.show(io::IO, f::SeparableSum)  
-
-	println(io,"Separable Sum: \n")
-	if length(f.prox_col)<=4
-		for p in f.prox_col
-			show(p)
-			println(io,"\n")
-		end
-	else
-		show(f.prox_col[1])
-		println(io,"\n .... \n")
-		show(f.prox_col[end])
-	end
+fun_name(f::SeparableSum) = "separable sum"
+function fun_dom(f::SeparableSum)
+	# s = ""
+	# for k in eachindex(f.fs)
+	# 	s = string(s, fun_dom(f.fs[k]), " × ")
+	# end
+	# return s
+	return "n/a" # for now
+end
+fun_expr(f::SeparableSum) = "(x₁,…,xᵣ) ↦ f[1](x₁) + … + f[r](xᵣ)"
+function fun_params(f::SeparableSum)
+	# s = "r = $(size(f.fs)), "
+	# for k in eachindex(f.fs)
+	# 	s = string(s, "f[$(k)] = $(typeof(f.fs[k])), ")
+	# end
+	# return s
+	return "n/a" # for now
 end
--- test/test_SeparableSum.jl
+++ test/test_SeparableSum.jl
@@ -1,28 +1,24 @@
-
-using Base.Test
-using ProximalOperators
-
 srand(123)
+
 x = randn(10)
 X = randn(10,10)+im*randn(10,10)
 y0 = randn(size(x))
 Y0 = randn(size(X))+im*randn(size(X))
-y,Y = copy(y0),copy(Y0)
+y, Y = copy(y0),copy(Y0)
 
-lambdas = [abs(randn(size(x))),0.1]
-prox_col = [NormL1(lambdas[1]),NormL2(lambdas[2])]
+lambdas = [abs(randn(size(x))), 0.1]
+prox_col = [NormL1(lambdas[1]), NormL2(lambdas[2])]
 
-#testing constructors
+# testing constructors
 f = SeparableSum(prox_col)
-f = SeparableSum([NormL1,NormL2],lambdas)
-y,fy = prox(f,[x,X],1.)
+y, fy = prox(f, [x, X], 1.)
 
-y1,fy1 = prox(prox_col[1],x,1.)
-y2,fy2 = prox(prox_col[2],X,1.)
+y1, fy1 = prox(prox_col[1], x, 1.)
+y2, fy2 = prox(prox_col[2], X, 1.)
 
 @test norm((fy1+fy2)-fy)<1e-11
 @test norm(y[1]-y1)<1e-11
 @test norm(y[2]-y2)<1e-11
 
-show(f)
-show(SeparableSum(repmat(prox_col,3)))
+println(f)
+println(SeparableSum(repmat(prox_col,3)))
