5040bfe7d856ffe95c52dfcc59965ec741204bf1
Lorenzo Stella <lorenzostella@gmail.com>
2016-12-23 19:05:17 +0100

    Added function tilting

---
+++ src/calculus/tilt.jl
@@ -0,0 +1,27 @@
+# Tilting (addition with affine function)
+
+immutable Tilt{T <: ProximableFunction, S <: AbstractArray, R <: Real} <: ProximableFunction
+  f::T
+  a::S
+  b::R
+end
+
+fun_dom(f::Tilt) = fun_dom(f.f)
+
+Tilt{T <: ProximableFunction, S <: AbstractArray}(f::T, a::S) = Tilt{T, S, eltype(a)}(f, a, 0.0)
+
+function (g::Tilt){T <: RealOrComplex}(x::AbstractArray{T})
+  return g.f(x) + vecdot(g.a, x) + g.b
+end
+
+function prox!{T <: RealOrComplex}(g::Tilt, x::AbstractArray{T}, y::AbstractArray{T}, gamma::Real=1.0)
+  v = prox!(g.f, x - gamma*g.a, y, gamma)
+  return v + vecdot(g.a, y) + g.b
+end
+
+function prox_naive{T <: RealOrComplex}(g::Tilt, x::AbstractArray{T}, gamma::Real=1.0)
+  y, v = prox_naive(g.f, x - gamma*g.a, gamma)
+  return y, v + vecdot(g.a, y) + g.b
+end
+
+is_prox_accurate(f::Tilt) = is_prox_accurate(f.f)
