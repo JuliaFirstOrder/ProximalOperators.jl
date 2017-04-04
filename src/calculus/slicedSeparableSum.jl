b8fbf4c5abc26f75b0b74987c14cf9794050ca57
nantonel <nantonel@esat.kuleuven.be>
2017-04-04 17:12:45 +0200

    simplified SlicedSeparableSum

--- src/calculus/slicedSeparableSum.jl
+++ src/calculus/slicedSeparableSum.jl
@@ -2,56 +2,38 @@
 
 immutable SlicedSeparableSum{S <: AbstractArray, T <: AbstractArray} <: ProximableFunction
 	fs::S
-	is::T
-	dim::Integer
-	function SlicedSeparableSum(a::S, b::T, dim::Integer)
-		if size(a) != size(b)
-			error("size(fs) must coincide with size(is)")
+	idxs::T
+	function SlicedSeparableSum(fs, idxs)
+		if size(fs) != size(idxs)
+			error("size(fs) must coincide with size(idxs)")
 		else
-			new(a, b, dim)
+			verify_idx = true
+			for i in eachindex(idxs)
+				verify_idx *=
+				all([typeof(t) <: AbstractArray{Int64,1} || 
+	                             typeof(t) <: Colon for t in idxs[i]])
+			end
+			verify_idx ? new(fs, idxs) :error("invalid index")
 		end
 	end
 end
 
-SlicedSeparableSum{S <: AbstractArray, T <: AbstractArray}(a::S, b::T, dim::Integer=1) =
-	SlicedSeparableSum{S, T}(a, b, dim)
-
-function SlicedSeparableSum{T <: Tuple}(ps::AbstractArray{T}, dim::Integer=1)
-	a = Array{ProximableFunction}(length(ps))
-	b = Array{AbstractArray}(length(ps))
-	for i in eachindex(ps)
-		a[i] = ps[i][1]
-		b[i] = ps[i][2]
-	end
-	SlicedSeparableSum{typeof(a), typeof(b)}(a, b, dim)
-end
-
-function SlicedSeparableSum{P <: Pair}(p::AbstractArray{P}, dim::Integer = 1)
-	a = Vector{ProximableFunction}(length(p))
-	b = Vector{AbstractArray}(length(p))
-	for i in eachindex(p)
-		a[i] = p[i].first
-		b[i] = p[i].second
-	end
-	return SlicedSeparableSum(a, b, dim)
-end
+SlicedSeparableSum{S <: AbstractArray, T <: AbstractArray}(a::S, b::T) =
+SlicedSeparableSum{S, T}(a, b)
 
 function (f::SlicedSeparableSum{S}){S <: AbstractArray, T <: AbstractArray}(x::T)
 	sum = 0.0
-  for k in eachindex(f.fs)
-		sum += f.fs[k](x[is[k]])
+	for k in eachindex(f.fs)
+		sum += f.fs[k](x[f.idx[k]])
 	end
 	return sum
 end
 
-function prox!{T <: RealOrComplex}(y::AbstractArray{T}, f::SlicedSeparableSum, x::AbstractArray{T}, gamma::Real=1.0)
+function prox!{T <: RealOrComplex}(y::AbstractArray{T}, 
+				   f::SlicedSeparableSum, x::AbstractArray{T}, gamma::Real=1.0)
   v = 0.0
-  nd = ndims(x)
-
-  for i in eachindex(f.fs)
-	  z = slicedim(y, f.dim, f.is[i])
-	  g = prox!(z, f.fs[i], slicedim(x, f.dim, f.is[i]), gamma)
-	  y[[ n==f.dim ? f.is[i] : indices(y, n) for n in 1:nd ]...] = z
+  for k in eachindex(f.fs)
+	  g = prox!(view(y,f.idxs[k]...), f.fs[k], view(x,f.idxs[k]...), gamma)
 	  v += g
   end
   return v
