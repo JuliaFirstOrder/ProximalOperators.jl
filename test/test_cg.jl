52735e9205e321eb431693c408827b120d1eae0d
Lorenzo Stella <lorenzostella@gmail.com>
2017-07-02 16:00:18 +0200

    updated cg

--- test/test_cg.jl
+++ test/test_cg.jl
@@ -2,7 +2,7 @@
 
 @printf("\nTesting CG routines\n")
 
-n, m = 500, 1000
+n, m = 100, 200
 B = randn(n, m)
 A = B*B';
 x_star = randn(n)
@@ -14,9 +14,25 @@ it = ProximalOperators.cg!(x, A, 0.0, b, 1e-10)
 @test norm(A*x - b) <= 1e-8
 @test norm(x - x_star) <= 1e-6
 
+d = diag(A)
+it_p1 = ProximalOperators.cg!(x, A, 0.0, b, 1e-10, d)
+
+F = lufact(A)
+it_p2 = ProximalOperators.cg!(x, A, 0.0, b, 1e-10, F)
+
+@test it_p2 == 1
+
 c = A*x_star + 0.5*x_star
 
 it = ProximalOperators.cg!(x, A, 0.5, c, 1e-10)
 
 @test norm(A*x + 0.5*x - c) <= 1e-8
 @test norm(x - x_star) <= 1e-6
+
+d = diag(A) .+ 0.5
+it_p1 = ProximalOperators.cg!(x, A, 0.5, b, 1e-10, d)
+
+F = lufact(A + 0.5*I)
+it_p2 = ProximalOperators.cg!(x, A, 0.5, c, 1e-10, F)
+
+@test it_p2 == 1
