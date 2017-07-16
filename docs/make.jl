3b4700a08da0ea6d37741c22eefbb81643f4f9fc
Lorenzo Stella <lorenzostella@gmail.com>
2017-07-17 01:55:46 +0200

    automated docs deploy?

--- docs/make.jl
+++ docs/make.jl
@@ -1,14 +1,19 @@
 using Documenter, ProximalOperators
 
-makedocs(modules = [ProximalOperators],
-         format = :html,
-         sitename = "ProximalOperators.jl",
-         authors = "Lorenzo Stella",
-         pages = Any[
-         "Home" => "index.md",
-         "Functions" => "functions.md",
-         "Calculus rules" => "calculus.md",
-         "Prox and gradient" => "operators.md",
-         "Demos" => "demos.md"
-         ]
-         )
+makedocs(
+  modules = [ProximalOperators],
+  format = :html,
+  sitename = "ProximalOperators.jl",
+  authors = "Lorenzo Stella",
+  pages = Any[
+  "Home" => "index.md",
+  "Functions" => "functions.md",
+  "Calculus rules" => "calculus.md",
+  "Prox and gradient" => "operators.md",
+  "Demos" => "demos.md"
+  ]
+)
+
+deploydocs(
+   repo   = "github.com/kul-forbes/ProximalOperators.jl.git"
+)
