baebd06ce600508294645f1d44b27979fae48367
Lorenzo Stella <lorenzostella@gmail.com>
2017-07-17 03:16:31 +0200

    fixed docs config

--- docs/make.jl
+++ docs/make.jl
@@ -5,7 +5,7 @@ makedocs(
 )
 
 deploydocs(
-  deps   = Deps.pip("pygments", "mkdocs", "python-markdown-math"),
+  deps   = Deps.pip("pygments", "mkdocs", "python-markdown-math", "mkdocs-bootstrap"),
   repo   = "github.com/kul-forbes/ProximalOperators.jl.git",
   julia  = "0.6",
   osname = "linux"
