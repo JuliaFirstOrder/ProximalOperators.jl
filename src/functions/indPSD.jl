4cb3656b3198c4901ad7c374ee213f269cfebc35
Mattias Fält <mattiasf@control.lth.se>
2017-07-19 18:37:11 +0200

    PSD scaling

--- src/functions/indPSD.jl
+++ src/functions/indPSD.jl
@@ -5,7 +5,7 @@ export IndPSD
 """
 **Indicator of the set of positive semi-definite cone**
 
-    IndPSD()
+    IndPSD(;scaling=false)
 
 Returns the indicator of the set
 ```math
@@ -13,9 +13,26 @@ C = \\{ X : X \\succeq 0 \\}.
 ```
 The argument to the function can be either a `Symmetric` or `Hermitian` object,
 or an object of type `AbstractVector{Float64}` holding a symmetric matrix in (lower triangular) packed storage.
+
+If `scaling = true` then the vectors `y` and `x` in
+`prox!(y::AbstractVector{Float64}, f::IndPSD, x::AbstractVector{Float64}, args...)`
+have the off-diagonal elements multiplied with `√2` to preserve inner products,
+see Vandenberghe 2010: http://www.seas.ucla.edu/~vandenbe/publications/coneprog.pdf .
+
+I.e. when when `scaling=true`, let `X,Y` be matrices and
+
+`x = (X_{1,1}, √2⋅X_{2,1}, ... ,√2⋅X_{n,1}, X_{2,2}, √2⋅X_{3,2}, ..., X_{n,n})`,
+
+`y = (Y_{1,1}, √2⋅Y_{2,1}, ... ,√2⋅Y_{n,1}, Y_{2,2}, √2⋅Y_{3,2}, ..., Y_{n,n})`
+
+then `prox!(Y, f, X)` is equivalent to `prox!(y, f, x)`.
 """
 
-immutable IndPSD <: ProximableFunction end
+immutable IndPSD <: ProximableFunction
+    scaling::Bool
+end
+
+IndPSD(;scaling=false) = IndPSD(scaling)
 
 function (f::IndPSD){T <: RealOrComplex}(X::HermOrSym{T})
   F = eigfact(X);
@@ -58,10 +75,27 @@ function prox_naive{T <: RealOrComplex}(f::IndPSD, X::HermOrSym{T}, gamma::Real=
   return F.vectors * diagm(max.(0.0, F.values)) * F.vectors', 0.0;
 end
 
+"""
+Scales the diagonal of `x` with `val`, where `x` is the lower triangualar part
+of a matrix, stored column by column.
+"""
+function scale_diagonal!(x, val)
+    n = Int(sqrt(1/4+2*length(x))-1/2)
+    k = -n
+    for i = 1:n
+         k += n - i + 2    #Calculate indices of diagonal elements recursively (paralell faster?)
+         x[k] .*= val      #Scale diagonal
+    end
+    return
+end
+
 ### Below: with AbstractVector argument
 
 function (f::IndPSD){T <: Float64}(x::AbstractVector{T})
-  Z = dspev!('N', 'L', copy(x))
+  y = copy(x)
+  f.scaling && scale_diagonal!(y, sqrt(2)) #If scaling, scale diagonal (eigenvalues scaled by sqrt(2))
+
+  Z = dspev!('N', 'L', y)
   for i in 1:length(Z)
     #Do we allow for some tolerance here?
     if Z[i] <= -1e-14
@@ -73,6 +107,9 @@ end
 
 function prox!(y::AbstractVector{Float64}, f::IndPSD, x::AbstractVector{Float64}, gamma::Real=1.0)
   y[:] = x              # Copy x since dspev! corrupts input
+
+  f.scaling && scale_diagonal!(y, sqrt(2)) #If scaling, scale diagonal
+
   (W, Z) = dspevV!('L', y)
   W = max.(W, 0.0)         # NonNeg eigenvalues
   M = Z.*W'             # Equivalent to Z*diagm(W) without constructing W matrix
@@ -83,6 +120,9 @@ function prox!(y::AbstractVector{Float64}, f::IndPSD, x::AbstractVector{Float64}
     y[k] = M[i,j]
     k = k+1
   end
+
+  f.scaling && scale_diagonal!(y, 1/sqrt(2))  #If scaling, un-scale diagonal
+
   return 0.0
 end
 
@@ -97,7 +137,22 @@ function prox_naive{T<:Float64}(f::IndPSD, x::AbstractVector{T}, gamma::Real=1.0
     end
     k = k+1
   end
+  # Scale diagonal elements by sqrt(2)
+  # See Vandenberghe 2010 http://www.seas.ucla.edu/~vandenbe/publications/coneprog.pdf
+  # It's equivalent to scaling off-diagonal by 1/sqrt(2) and working with sqrt(2)*X
+  if f.scaling
+    for i = 1:n
+      X[i,i] .*= sqrt(2)
+    end
+  end
   X, v = prox_naive(f, Symmetric(X), gamma)
+
+  if f.scaling  #Scale diagonal elements back
+    for i = 1:n
+      X[i,i] ./= sqrt(2)
+    end
+  end
+
   y = similar(x)
   k = 1
   for j = 1:n, i = j:n                # Store Lower half of X in y
