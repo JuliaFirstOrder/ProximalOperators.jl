5e34c738c0fb1c6951169fcc10c5282dab32f773
Lorenzo Stella <lorenzostella@gmail.com>
2017-08-27 22:04:18 +0200

    fixed docstring

--- src/functions/indGraph.jl
+++ src/functions/indGraph.jl
@@ -7,13 +7,16 @@ export IndGraph
 
 For matrix `A` (dense or sparse) returns the indicator function of the set
 ```math
-S = \\{(x, y) : Ax = y\\}.
+G_A = \\{(x, y) : Ax = y\\}.
 ```
 The evaluation of `prox!` uses direct methods based on LDLt (LL for dense cases) matrix factorization and backsolve.
 
-The `prox!` method operates on pairs `(x, y)` as input/output. So if `f = IndGraph(A)` is the indicator of affine space ``S``,
-while `(x, y)` and `(c, d)` are pairs of vectors of the same sizes, then `prox!((c, d), f, (x, y))`
-writes to `(c, d)` the projection onto ``S`` of `(x, y)`.
+The `prox!` method operates on pairs `(x, y)` as input/output. So if `f = IndGraph(A)` is the indicator of the graph ``G_A``,
+while `(x, y)` and `(c, d)` are pairs of vectors of the same sizes, then
+```
+prox!((c, d), f, (x, y))
+```
+writes to `(c, d)` the projection onto ``G_A`` of `(x, y)`.
 """
 
 abstract type IndGraph <: ProximableFunction end
