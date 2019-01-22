156b6b67a5bcb7f0f63ba149e4ba5e145eac6328
Lorenzo Stella <stellalo@amazon.com>
2019-01-23 00:13:00 +0100

    fixing docs?

--- docs/make.jl
+++ docs/make.jl
@@ -1,24 +1,18 @@
 using Documenter, ProximalOperators
 
 makedocs(
-  modules = [ProximalOperators],
-  format = :html,
-  sitename = "ProximalOperators.jl",
-  authors = "Lorenzo Stella and contributors",
-  pages = Any[
-  "Home" => "index.md",
-  "Functions" => "functions.md",
-  "Calculus rules" => "calculus.md",
-  "Prox and gradient" => "operators.md",
-  "Demos" => "demos.md"
-  ],
+    modules = [ProximalOperators],
+    sitename = "ProximalOperators.jl",
+    pages = [
+        "Home" => "index.md",
+        "Functions" => "functions.md",
+        "Calculus rules" => "calculus.md",
+        "Prox and gradient" => "operators.md",
+        "Demos" => "demos.md"
+    ],
 )
 
 deploydocs(
-  repo   = "github.com/kul-forbes/ProximalOperators.jl.git",
-  julia  = "0.6",
-  osname = "linux",
-  target = "build",
-  deps = nothing,
-  make = nothing,
+    repo   = "github.com/kul-forbes/ProximalOperators.jl.git",
+    versions = "v#.#",
 )
