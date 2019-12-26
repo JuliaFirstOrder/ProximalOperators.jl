b0a2085597671d51211f67686f5a8c50e0d00e81
zeptodoctor <44736852+zeptodoctor@users.noreply.github.com>
2019-12-26 20:36:22 +0000

    build based on 763eb68

--- dev/search_index.js
+++ dev/search_index.js
@@ -573,7 +573,7 @@ var documenterSearchIndex = {"docs": [
     "page": "Calculus rules",
     "title": "ProximalOperators.Precompose",
     "category": "type",
-    "text": "Precomposition with linear mapping/translation\n\nPrecompose(f, L, μ, b)\n\nReturns the function\n\ng(x) = f(Lx + b)\n\nwhere f is a convex function and L is a linear mapping: this must satisfy LL^* = μI for μ  0. Furthermore, either f is separable or parameter μ is a scalar, for the prox of g to be computable.\n\nParameter L defines L through the mul! method. Therefore L can be an AbstractMatrix for example, but not necessarily.\n\nIn this case, prox and prox! are computed according to Prop. 24.14 in Bauschke, Combettes \"Convex Analisys and Monotone Operator Theory in Hilbert Spaces\", 2nd edition, 2016. The same result is Prop. 23.32 in the 1st edition of the same book.\n\n\n\n\n\n"
+    "text": "Precomposition with linear mapping/translation\n\nPrecompose(f, L, μ, b)\n\nReturns the function\n\ng(x) = f(Lx + b)\n\nwhere f is a convex function and L is a linear mapping: this must satisfy LL^* = μI for μ  0. Furthermore, either f is separable or parameter μ is a scalar, for the prox of g to be computable.\n\nParameter L defines L through the mul! method. Therefore L can be an AbstractMatrix for example, but not necessarily.\n\nIn this case, prox and prox! are computed according to Prop. 24.14 in Bauschke, Combettes \"Convex Analysis and Monotone Operator Theory in Hilbert Spaces\", 2nd edition, 2016. The same result is Prop. 23.32 in the 1st edition of the same book.\n\n\n\n\n\n"
 },
 
 {
