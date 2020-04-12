16e0373a5c3c430d365e074d0829ca84b119c1fa
Lorenzo Stella <lorenzostella@gmail.com>
2020-04-12 21:22:23 +0200

    Update benchmarks.jl

--- benchmark/benchmarks.jl
+++ benchmark/benchmarks.jl
@@ -7,7 +7,7 @@ const SUITE = BenchmarkGroup()
 k = "IndPSD"
 SUITE[k] = BenchmarkGroup(["IndPSD"])
 for T in [Float32, Float64, Complex{Float32}, Complex{Float64}]
-    for n in [3, 10, 20, 50]
+    for n in [10, 20, 50]
         SUITE[k][T, n] = @benchmarkable prox!(Y, f, X) setup=begin
             f = IndPSD()
             W = if $T <: Real
